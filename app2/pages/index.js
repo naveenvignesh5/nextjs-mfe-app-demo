a9ca7942c5ea117b96ed4306254352cf8fbb104c
Naveen Vignesh <naveenvignesh5.nv@gmail.com>
2021-01-07 20:55:45 +0530

    updates

--- app2/pages/index.js
+++ app2/pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 
-// We need to use top level await on these modules as they are async. 
+// We need to use top level await on these modules as they are async.
 // This is actually what let's module federation work with NextJS
 const Nav = (await import('app1/nav')).default;
 const add = (await import('app1/add')).default;
@@ -45,14 +45,16 @@ export default function Home() {
 
           <a
             href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}>
+            className={styles.card}
+          >
             <h3>Examples &rarr;</h3>
             <p>Discover and deploy boilerplate example Next.js projects.</p>
           </a>
 
           <a
             href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}>
+            className={styles.card}
+          >
             <h3>Deploy &rarr;</h3>
             <p>
               Instantly deploy your Next.js site to a public URL with Vercel.
@@ -65,7 +67,8 @@ export default function Home() {
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
-          rel="noopener noreferrer">
+          rel="noopener noreferrer"
+        >
           Powered by{' '}
           <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
         </a>
